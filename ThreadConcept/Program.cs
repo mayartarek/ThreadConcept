a3234351cd9d2ce70a125258660c6df3ddeb878c
mayartarek <mayartarek58@gmail.com>
2026-09-09 12:18:04 +0300

    Initial solution setup with threading and sample projects
    Added .gitignore for Visual Studio artifacts. Created ThreadConcept.slnx with DivideConqure and ThreadConcept projects targeting .NET 8.0. Implemented a multithreading example in ThreadConcept and a basic "Hello, World!" in DivideConqure.

---
+++ ThreadConcept/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more inform
+namespace ThreadConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Before");
+            Console.WriteLine(Thread.CurrentThread.Name);
+           var t1= new Thread(BoilingWater);
+            var t2= new Thread(BoilingWater);
+            t1.Priority = ThreadPriority.Highest;
+            t2.Priority = ThreadPriority.Lowest;
+            Thread.CurrentThread.Priority=ThreadPriority.BelowNormal;
+            t1.Name = "Boiling Water Thread 1";
+            t2.Name = "Boiling Water Thread 2";
+            Thread.CurrentThread.Name = "Main Thread";
+            t1.Start();
+            t2.Start ();
+            BoilingWater();
+
+        }
+
+        private static void BoilingWater() 
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+            }
+
+        }
+        private static void OpenDoor()
+        {
+            Console.WriteLine("Opening the door... ");
+        }
+    }
+}
