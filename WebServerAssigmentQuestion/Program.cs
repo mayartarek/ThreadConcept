ee43677ac97bea21169820ea567df25ddefc380d
mayartarek <mayartarek58@gmail.com>
2026-09-14 00:23:37 +0300

    Add WebServerAssigmentQuestion project with threading demo
    Added WebServerAssigmentQuestion project to the solution. Implemented a multithreaded input processing system in Program.cs using a queue and background threads to handle user input concurrently. Project targets .NET 8.0.

---
+++ WebServerAssigmentQuestion/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+ Queue<string?> inputQueue = new Queue<string?>();
+Thread thread = new Thread(() =>enqueueInput());
+thread.Start();
+Console.WriteLine("Hello, World!");
+while (true)
+{
+    string? input = Console.ReadLine();
+    if(input?.ToLower() == "exist")
+    {
+       break;
+    }
+    inputQueue.Enqueue(input);
+}
+ void enqueueInput()
+{
+    while (true)
+    {
+        if(inputQueue.Count > 0) 
+        {
+            string input = inputQueue.Dequeue();
+            var task = new Thread(() => ProcessInput(input));
+            task.Start();
+        }
+      
+    }
+}
+ static void ProcessInput(string input)
+{
+    Thread.Sleep(1000); // Simulate a delay in processing
+    // Simulate processing the input
+    Console.WriteLine($"Processing input: {input}");
+}
